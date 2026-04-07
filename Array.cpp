daf5eefb4a2dc923c0ca351dd76619fc5930420b
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-04-07 15:14:38 +0700

    isi data perulangan

--- Array.cpp
+++ Array.cpp
@@ -32,5 +32,16 @@ cout << "Data ke -3 : " << datamu[2] << endl;
 cout << "Data ke -4 : " << datamu[3] << endl;
 cout << "Data ke -5 : " << datamu[4] << endl;
 
+cout << endl;
 
+for (int i = 0; i <= 4; i++) {
+    cout << "Data index ke - " << i << " : ";
+    cin >> datamu[i];
+}
+cout << endl;
+for (int i = 0; i <= 4; i++) {
+    cout << "Data ke - " << i+1 << " : " << datamu[i] << endl;
+}
+
+    return 0;
 }
